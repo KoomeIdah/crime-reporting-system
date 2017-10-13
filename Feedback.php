ab5f74024206ba0023e5866072bb167a493d7092
KoomeIdah <32255056+KoomeIdah@users.noreply.github.com>
2017-10-13 11:59:17 +0300

    Add files via upload

---
+++ Feedback.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+         <div class="main">
+    <div class="icon"></div>
+    <h1 style="text-align: center;">KENYA POLICE SERVICE</h1>
+    <link rel="shortcut icon" type="image/x-icon" href="pictures/Kenya_Police_Flag.gif"/>
+    
+    <div class="rule">
+       
+    </div>
+    <div class="rule2"></div>
+        <div class="content">
+            <section>
+                <p>FEEDBACK MODULE.</p>
+     <form >
+         Case <br><input class="text"><br>
+    Officer Number <br><br><input type="text"><br>
+    Reporter Name: <br><input type="text"><br>
+    <div class="textarea">Case Report</div>
+    <textarea rows="6" cols="100" name="Case Description" required> </textarea>
+    <br><br>
+    <div class="textarea"> Add any Evidence, if available. </div><br>
+   
+<input class="btn btn-7h" name="submit" type="submit" value="ENTER" />
+</form>  
+        </section></div>
+    </div>
+    
+
+</body>
+</html>
+    </body>
+</html>
